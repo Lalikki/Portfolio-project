c71d26a1a734de9dd4661c6ad11b9eac6be7154a
Lalikki <laurijarvinen@hotmail.com>
2021-08-30 14:16:04 +0300

    routes & some fixes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import 'font-awesome/css/font-awesome.min.css';
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Experience from "./components/Experience";
+import Portfolio from "./components/Portfolio";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -17,11 +19,18 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Home />
-
           </Route>
 
           <Route path="/about">
             <About />
+          </Route>
+
+          <Route path="/experience">
+            <Experience />
+          </Route>
+
+          <Route path="/portfolio">
+            <Portfolio />
           </Route>
         </Switch>
 
---
+++ src/components/Experience.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Experience = () => {
+    return (
+        <div>
+            <h2>My experience</h2>
+        </div>
+    )
+}
+
+export default Experience;
